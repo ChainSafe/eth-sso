99a8f92528a7c7164591730593f27d71e5b83e3c
Gregory Markou <gregorymarkou@gmail.com>
2023-11-09 20:40:31 +0200

    basic structure

---
+++ src/app/passkey/getPasskey.tsx
@@ -0,0 +1,15 @@
+const getPasskeyCredential = async (challenge: string) => {
+    const challengeBuffer = Uint8Array.from(challenge, (c) => c.charCodeAt(0));
+    const publicKeyCredentialRequestOptions: PublicKeyCredentialRequestOptions = {
+      challenge: challengeBuffer,
+      rpId: process.env.REACT_APP_RP_DOMAIN,
+      userVerification: "preferred",
+      timeout: 60000,
+    };
+  
+    return await navigator.credentials.get({
+      publicKey: publicKeyCredentialRequestOptions,
+    });
+  };
+  
+  export default getPasskeyCredential;
---
+++ src/app/sso/index.tsx
@@ -0,0 +1,13 @@
+export const parseUrl = () => {
+    const queryParameters = new URLSearchParams(window.location.search)
+    const redirectUri = queryParameters.get("redirect_uri");
+    const chainId = queryParameters.get("chain_id");
+    if (redirectUri == null || chainId == null) {
+        return null;
+    }
+    else return {redirectUri, chainId};
+}
+
+export const redirect = (redirectUri: string, signer_uid: string, smartAccountAddress: string) => {
+    window.location.replace(`http://${redirectUri}/?signer_uid=${signer_uid}&smart_account_address=${smartAccountAddress}`);
+}
