42bda8aa0d1a7d0d49da7070347472327a10dbc7
BeroBurny <bero4net@gmail.com>
2024-02-20 15:19:06 +0100

    v0.1

--- examples/sso-provider/src/app/_components/SendTransaction.tsx
+++ examples/sso-provider/src/app/_components/SendTransaction.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback } from "react";
 import { redirect } from "next/navigation";
+import { Transaction } from "web3-eth-accounts";
 import type { SendTransactionRequestSchema } from "@/sendTransaction/types";
 import { useWeb3Modal } from "@/hooks/useWeb3Modal";
 
@@ -12,30 +13,39 @@ export default function SendTransaction({
   chain_id,
   transaction,
 }: Props): JSX.Element {
-  const [provider] = useWeb3Modal(chain_id);
+  const [provider, isLoading, privateKey] = useWeb3Modal(chain_id);
 
   const onClick = useCallback(() => {
-
-
+    // TODO: make it works
+    const transactionData = Transaction.fromSerializedTx(
+      Uint8Array.from(Buffer.from(transaction.substring(2), "hex")),
+    );
     void provider
       .sendAsync({
         jsonrpc: "2.0",
         method: "eth_sendRawTransaction",
-        params: [transaction],
+        params: [
+          transactionData.sign(Uint8Array.from(Buffer.from(privateKey, "hex"))),
+        ],
         id: 1,
       })
       .then((tx) => {
-        console.log(tx);
+        const url = new URL("sendTransaction", redirect_uri);
+        url.searchParams.set("signer_key", "");
+        url.searchParams.set("smart_account_address", "");
+        url.searchParams.set("tx_success", String(true));
+        url.searchParams.set("tx_hash", "");
 
-        const url = new URL(redirect_uri);
-
-        redirect(`${redirect_uri}`);
+        redirect(url.toString());
       });
-  }, [chain_id, transaction, provider]);
+  }, [chain_id, transaction, provider, privateKey]);
 
   return (
     <div>
-      <button onClick={onClick}>Submit Transaction</button>
+      <button disabled={isLoading} onClick={onClick}>
+        Submit Transaction
+      </button>
+      <br />
       <br />
       <p>{redirect_uri}</p>
       <p>{chain_id}</p>
--- examples/sso-provider/src/hooks/useWeb3Modal.ts
+++ examples/sso-provider/src/hooks/useWeb3Modal.ts
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import { Web3Auth } from "@web3auth/modal";
 import type { IProvider } from "@web3auth/base";
+import type { HexString } from "web3";
 import { WEB3_AUTH_ID } from "@/lib/constants";
 
-type Return = [IProvider, false, Web3Auth] | [null, true, null];
+type Return =
+  | [IProvider, false, HexString, Web3Auth]
+  | [null, true, null, null];
 
 export function useWeb3Modal(chainId: string): Return {
   const [web3Auth, setWeb3Auth] = useState<Web3Auth | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [provider, setProvider] = useState<IProvider | null>();
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [privateKey, setPrivateKey] = useState<HexString | null>(null);
 
   useEffect(() => {
     const instance = new Web3Auth({
@@ -24,13 +28,20 @@ export function useWeb3Modal(chainId: string): Return {
     void instance
       .initModal()
       .then(() => instance.connect())
-      .then(setProvider)
-      .then(() => {
-        setIsLoading(false);
+      .then((web3AuthProvider) => {
+        setProvider(web3AuthProvider);
+        void web3AuthProvider
+          .request({
+            method: "eth_private_key", // use "private_key" for other non-evm chains
+          })
+          .then(setPrivateKey)
+          .then(() => {
+            setIsLoading(false);
+          });
       });
     setWeb3Auth(instance);
   }, [chainId]);
 
-  if (isLoading) return [null, true, null];
-  return [provider, false, web3Auth];
+  if (isLoading) return [null, true, null, null];
+  return [provider, false, privateKey, web3Auth];
 }
