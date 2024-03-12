ae840362fa23f21aef1b9fe148b7d191fcea8175
BeroBurny <bero4net@gmail.com>
2024-03-12 15:43:45 +0100

    remove passkey code

--- examples/react-demo/src/app/_components/AccountDetails.tsx
+++ examples/react-demo/src/app/_components/AccountDetails.tsx
@@ -3,15 +3,13 @@ import type { ReactElement } from "react";
 interface Props {
   selectedSSOProvider: string;
   smartAccountAddress: string;
-  signerKey: string;
 }
 
 export function AccountDetails({
   selectedSSOProvider,
   smartAccountAddress,
-  signerKey,
 }: Props): ReactElement {
-  if (!selectedSSOProvider || !smartAccountAddress || !signerKey)
+  if (!selectedSSOProvider || !smartAccountAddress)
     return <div />;
 
   return (
@@ -22,7 +20,6 @@ export function AccountDetails({
       {smartAccountAddress && (
         <p>Smart Contract Account address: {smartAccountAddress}</p>
       )}
-      {signerKey && <p>Owner key: {signerKey}</p>}
     </div>
   );
 }
