f67ce1208a700813755e66bf38777b5d4e329766
BeroBurny <bero4net@gmail.com>
2024-03-27 11:24:12 +0100

    figure it out post message is not working on cross-origin

--- packages/common/src/events.ts
+++ packages/common/src/events.ts
@@ -1,35 +1,53 @@
-import {ProviderSelected, Transaction, UserAccount} from "./types";
+import type { ProviderSelected, Transaction, UserAccount } from "./types";
 
-/** Work around for NextJS where `CustomEvent` does not exist */
-const EventClass = CustomEvent || Event;
+/** Work around for Next.js where `CustomEvent` does not exist */
+let EventClass: typeof CustomEvent;
+try {
+  EventClass = CustomEvent;
+} catch {
+  EventClass = Event as unknown as typeof CustomEvent;
+}
 
-export class ProviderSelectedEvent extends EventClass<ProviderSelected> {
+class ETHSSOBaseEventClass<Type> extends EventClass<Type> {
+  public stringify(): string {
+    return JSON.stringify(
+      {
+        type: this.type,
+        detail: this.detail,
+      },
+      null,
+      2,
+    );
+  }
+}
+
+export class ProviderSelectedEvent extends ETHSSOBaseEventClass<ProviderSelected> {
   constructor(providerUrl: string, name: string) {
-    super('providerSelected', {
-      detail: {url: providerUrl, name},
+    super("providerSelected", {
+      detail: { url: providerUrl, name },
     });
   }
 }
 
-export class UserAccountEvent extends EventClass<UserAccount> {
+export class UserAccountEvent extends ETHSSOBaseEventClass<UserAccount> {
   constructor(account: UserAccount) {
-    super('authenticationSuccess', {
+    super("authenticationSuccess", {
       detail: account,
     });
   }
 }
 
-export class TransactionEvent extends EventClass<Transaction> {
+export class TransactionEvent extends ETHSSOBaseEventClass<Transaction> {
   constructor(transaction: Transaction) {
-    super('transactionComplete', {
+    super("transactionComplete", {
       detail: transaction,
     });
   }
 }
 
-export class AbortedEvent extends EventClass<string> {
+export class AbortedEvent extends ETHSSOBaseEventClass<string> {
   constructor(reason: string) {
-    super('aborted', {
+    super("aborted", {
       detail: reason,
     });
   }
--- packages/consumer/src/utils.ts
+++ packages/consumer/src/utils.ts
@@ -4,31 +4,11 @@ export function windowOpen(url: string): WindowProxy | null {
   const left = window.innerWidth / 2 - width / 2;
   const top = window.innerHeight / 2 - height / 2;
 
-  const popUp = window.open(
+  return window.open(
     url,
     "",
-    `popup=yes toolbar=no, location=no, directories=no, status=no, menubar=no, 
+    `toolbar=no, location=no, directories=no, status=no, menubar=no, 
           scrollbars=no, resizable=yes, copyhistory=no, width=${width}, 
           height=${height}, top=${top}, left=${left}`,
   );
-
-  const opts = {capture: true};
-
-  ['pageshow', 'focus', 'blur', 'visibilitychange', 'resume'].forEach((type) => {
-    popUp?.addEventListener(type, (ev) => { console.log(type, ev)});
-  });
-  popUp?.addEventListener('freeze', (ev) => { console.log('freeze', ev)}, opts);
-  popUp?.addEventListener('pagehide', (ev) => { console.log('pagehide', ev)}, opts);
-  popUp?.addEventListener('unload', (ev) => { console.log('unload', ev)}, opts);
-
-
-  popUp?.addEventListener('load', (ev) => { console.log('load', ev)});
-
-  window.focus();
-
-  setTimeout(() => {
-    popUp?.focus();
-  }, 10000)
-
-  return popUp;
 }
