11baa4c550f9e113e7356f028a377a772ace3456
BeroBurny <bero4net@gmail.com>
2024-04-04 16:45:39 +0200

    fix lint

--- packages/react/src/hooks.ts
+++ packages/react/src/hooks.ts
@@ -7,17 +7,23 @@ export function useAuthCallback(
   useEffect(() => {
     const auth = parser.parseAuth(searchParams);
     if (auth) {
-      if (window.opener) window.opener.postMessage(auth.clone());
+      if (window.opener || "postMessage" in window.opener)
+        (window.opener as typeof window.parent).postMessage(auth.clone());
       else window.parent.postMessage(auth.clone());
     }
   }, [searchParams]);
 }
 
-export function useSendTransactionCallback(searchParams = new URLSearchParams(window.location.search)): void {
+export function useSendTransactionCallback(
+  searchParams = new URLSearchParams(window.location.search),
+): void {
   useEffect(() => {
     const transaction = parser.parseSendTransaction(searchParams);
     if (transaction) {
-      if (window.opener) window.opener.postMessage(transaction.clone());
+      if (window.opener || "postMessage" in window.opener)
+        (window.opener as typeof window.parent).postMessage(
+          transaction.clone(),
+        );
       else window.parent.postMessage(transaction.clone());
     }
   }, [searchParams]);
